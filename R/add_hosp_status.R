fb725eb42d74eb5d7b0d707e3e49093f8b74c2a2
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-02-22 14:04:55 +0000

    use correct columns for hospitalisations

--- R/add_hosp_status.R
+++ R/add_hosp_status.R
@@ -10,7 +10,9 @@
 add_hosp_status <- function(x) {
   hosp_status <- readr::read_csv(here::here(
     "data-truth", "OWID", "truth_OWID-Weekly Incident Hospitalizations.csv"
-  ), show_col_types = FALSE)
+  ), show_col_types = FALSE) |>
+    dplyr::mutate(target_variable = "inc hosp") |>
+    dplyr::select(target_variable, location, target_end_date, status)
   x <- x |>
     dplyr::left_join(
       hosp_status, by = c("target_variable", "location", "target_end_date")
