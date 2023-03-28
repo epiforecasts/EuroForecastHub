a830a8db8ce32d5fb7b12c15a791a274965be2a2
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-03-28 22:13:58 +0100

    improved grouping for all cases

--- R/convert_to_weekly.R
+++ R/convert_to_weekly.R
@@ -8,15 +8,16 @@
 ##' @author Sebastian Funk
 ##' @export
 convert_to_weekly <- function(x, ...) {
+  ## determine aggregation variables for filtering
+  aggregation_vars <- intersect(
+    colnames(x),
+    c("date", "value", "status", "snapshot_date", "type")
+  )
   x |>
     dplyr::mutate(sat_date = date_to_week_end(date, ...)) |>
-    dplyr::group_by(
-      dplyr::across(c(-date, -value, -status, -snapshot_date, -type))
-    ) |>
+    dplyr::group_by_at(vars(-aggregation_vars)) |>
     dplyr::mutate(n = dplyr::n()) |> ## count observations per Saturday date
-    dplyr::group_by(
-      dplyr::across(c(-date, -value, -status, -snapshot_date, -sat_date, -n))
-    )  |>
+    dplyr::group_by_at(vars(-aggregation_vars, -sat_date, -n)) |>
     ## check if data is weekly or daily
     dplyr::mutate(
       frequency = dplyr::if_else(all(n == 1), "weekly", "daily")
@@ -29,9 +30,7 @@ convert_to_weekly <- function(x, ...) {
     dplyr::mutate(date = dplyr::if_else(
       frequency == "weekly" & date + 6 == sat_date, date + 6, date
     )) |>
-    dplyr::group_by(
-      dplyr::across(c(-date, -value, -status, -snapshot_date, -type))
-    ) |>
+    dplyr::group_by_at(vars(-aggregation_vars)) |>
     dplyr::filter(date == max(date)) |>
     dplyr::ungroup() |>
     dplyr::mutate(
